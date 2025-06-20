590f972bf8ae07756085460bde33f6121926e0f2
x.j.hu <stormkoo@hotmail.com>
2025-06-20 09:52:04 +0800

    第二次提交

--- src/utils/Strings.php
+++ src/utils/Strings.php
@@ -6,6 +6,11 @@ class Strings
 {
     public static function join($name = "")
     {
-        return "hello " . $name;
+        return "hello - 1 - 2 " . $name;
+    }
+
+    public static function test($name = "")
+    {
+        return "composer go.";
     }
 }
